eb834332c3c8a088d5961cd2adbe851ab8079a5f
LynetteH09 <hernandezlynette303@gmail.com>
2025-01-15 23:42:45 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
-print("Welcome to my chatbox!")
-name = input("What is your name? ")
-age = input("Hello " + name + ", how old are you? ")
+def welcome_user():
+  print("Welcome to _____!")
+  print("I'm your friendly chatbot, here to help you.")
+    
+def get_user_info():
+  name = input("What's your name? ")
+  age = input("How old are you? ")
+  return name, age
+
+def display_menu():
+  print("How may I assist you today?")
+  print("1. PLaceholder")
+  print("2. PlaceHolder")
+  print("3. Placeholder")
+  print("4. PLaceholder")
+  print("5. Exit")
+  choice = input("Pick one of the option given, 1-5: ")
+
+def chatbot():
+  welcome_user()
+  get_user_info()
+  display_menu()
